9429f313142c137a21390975da6f92ef2a8018d6
y Nguyen <maingochaihung01@gmail.com>
2025-05-20 10:53:34 +0700

    fix maps

--- lib/constant/base_constant.dart
+++ lib/constant/base_constant.dart
@@ -4,5 +4,5 @@ class BaseConstants {
   static const VIETMAP_URL = "https://maps.vietmap.vn/api";
   static const GOOGLE_MAP_URL = "https://www.google.com/maps";
   static const VIET_MAP_APIKEY =
-      '62de0b2defa583bcd4f8fc3726810303ff9d13f246b9c94a';
+      '9b6f73bde8c0d0a19a79bf515817216a20600be23a668f40';
 }
